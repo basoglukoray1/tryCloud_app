fc9d5263d1604f1112c9ef0f82b1fbe79d7989d6
Koray Basoglu <basoglukoray1@gmail.com>
2022-04-08 17:35:11 -0400

    Jenkins-file update 3

--- src/test/java/com/trycloud/runners/CukesRunner.java
+++ src/test/java/com/trycloud/runners/CukesRunner.java
@@ -12,12 +12,13 @@ import org.junit.runner.RunWith;
                 "json:target/cucumber.json",
                 "html:target/cucumber-reports.html",
                 "rerun:target/rerun.txt",
+                "junit:target/junit/junit-report.xml",
                 "me.jvt.cucumber.report.PrettyReports:target/cucumber"
         },
         features = "src/test/resources/features",
         glue = "com/trycloud/step_definitions",
         dryRun = false,
-        tags = "",
+        tags = "@upload",
         publish = false
 
 )
